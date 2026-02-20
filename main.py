f2f9059c3a3d8d7032214741ee30ff77bf5f3dfc
h1zz <pavelrog315@gmail.com>
2026-02-20 18:06:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import os
 #Поменяй путь к файлу на свой, как его посмотреть я оставлю на страничке на гитхабе
-os.startfile('D:\zapret-discord-youtube-1.9.2 (1)\general (ALT9)')
+os.startfile('D:\Путь к файлу\Название Файла')
+
